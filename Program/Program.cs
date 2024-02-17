d68964f26498899f73e318f6dc4a412655732bb8
Oleg-githuber <oilminer@mail.ru>
2024-02-17 16:27:55 +0500

    Создал программу, которая принимает массив строк с клавиатуры

---
+++ Program/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите количество строк в массиве: ");
+int size1 = Int32.Parse(Console.ReadLine());
+string [] array1 = new string [size1];
+for (int i = 0; i < size1; i++) {
+    Console.WriteLine($"Введите строку №{i}");
+    array1[i] = Console.ReadLine();
+}
