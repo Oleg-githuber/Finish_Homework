bfa1255c4141ef0be6f4aba24c0ce14df784b6d8
Oleg-githuber <oilminer@mail.ru>
2024-02-17 16:35:06 +0500

    В цикле заполнения массива добавил вычисление размера второго массива

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,7 +1,12 @@
 ﻿Console.WriteLine("Введите количество строк в массиве: ");
-int size1 = Int32.Parse(Console.ReadLine());
-string [] array1 = new string [size1];
+int size1 = Int32.Parse(Console.ReadLine());    //Количество строк в первом массиве
+string [] array1 = new string [size1];  // Создание пустого массива
+int size2 = 0;  // Размер второго массива
+
+// Цикл заполнения массива с клавиатуры
 for (int i = 0; i < size1; i++) {
     Console.WriteLine($"Введите строку №{i}");
     array1[i] = Console.ReadLine();
+    if (array1[i].Length < 4) size2++;
 }
+
