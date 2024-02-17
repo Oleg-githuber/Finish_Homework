b0c0a67659f54dd6a7c7a90d3594c8d30d7154fc
Oleg-githuber <oilminer@mail.ru>
2024-02-17 21:06:08 +0500

    Добавил функции

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,4 +1,34 @@
-﻿Console.WriteLine("Введите количество строк в массиве: ");
+﻿// Функция вывода массива в консоль
+void WriteArray(string [] array1) {
+    Console.WriteLine("\nПервоначальный массив:");
+    Console.Write("[");
+    for (int i1 = 0; i1 < array1.Length; i1++) {
+        Console.Write($"\"{array1[i1]}\"");
+        if (i1 < (array1.Length - 1)) Console.Write(", ");
+    }
+    Console.Write("]");
+}
+
+// Функция формирования второго массива и вывода его в консоль
+string [] NewArray(string [] array1, string [] array2) {
+    int i2 = 0; // Индекс второго массива
+    Console.WriteLine("\nВаш новый массив:");
+    Console.Write("[");
+    for (int i1 = 0; i1 < array1.Length; i1++) {
+        if (array1[i1].Length < 4) {
+            array2[i2] = array1[i1];
+            Console.Write($"\"{array2[i2]}\"");
+            if (i2 < (array2.Length - 1)) Console.Write(", ");
+            i2++;
+        }
+    }
+    Console.Write("]");
+    return array2;
+}
+
+
+
+Console.WriteLine("Введите количество строк в массиве: ");
 int size1 = Int32.Parse(Console.ReadLine());    //Количество строк в первом массиве
 string [] array1 = new string [size1];  // Создание пустого массива
 int size2 = 0;  // Размер второго массива
@@ -13,30 +43,9 @@ for (int i = 0; i < size1; i++) {
 
 if (size2 == 0) Console.WriteLine("Массив не содержит коротких строк");
 else {
-    
-    // Цикл формирования второго массива
-    Console.WriteLine("\nПервоначальный массив:");
-    Console.Write("[");
-    for (int i1 = 0; i1 < size1; i1++) {
-        Console.Write($"\"{array1[i1]}\"");
-        if (i1 < (size1 - 1)) Console.Write(", ");
-    }
-    Console.Write("]");
-
-
+    WriteArray(array1);
     string [] array2 = new string [size2];
-    int i2 = 0; // Индекс второго массива
-    Console.WriteLine("\nВаш новый массив:");
-    Console.Write("[");
-    for (int i1 = 0; i1 < size1; i1++) {
-        if (array1[i1].Length < 4) {
-            array2[i2] = array1[i1];
-            Console.Write($"\"{array2[i2]}\"");
-            if (i2 < (size2 - 1)) Console.Write(", ");
-            i2++;
-        }
-    }
-    Console.Write("]");
+    NewArray(array1, array2);
 }
 
 Console.ReadLine();
